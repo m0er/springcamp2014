d979e8d505a9692475d8c3e019865e4ffe8aa56d
Yunseok Choi <ethdemor@gmail.com>
2014-06-03 01:01:48 +0900

    세션 디비에서 가져와 뿌려주는 코드 추가

--- session/views.py
+++ session/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render_to_response
+from session.models import *
 
 # Create your views here.
 def index(request, session_id):
+	session = Session.objects.get(id=session_id)
+
 	return render_to_response('session/index.html', {
-			'session_id': session_id
+			'session_id': session_id,
+			'session': session
 		})
