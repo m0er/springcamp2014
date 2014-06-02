af9e4dc5f0eef864ef5e3bf450e7c4ad62e06954
Yunseok Choi <ethdemor@gmail.com>
2014-06-03 00:31:08 +0900

    설치한 라이브러리 및 앱 추가

--- springcamp2014/settings.py
+++ springcamp2014/settings.py
@@ -36,6 +36,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'south',
+    'springcamp2014',
+    'speaker',
+    'session'
 )
 
 MIDDLEWARE_CLASSES = (
