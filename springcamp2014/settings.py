6629491d3a6d4bfd035ebf501a655babfe0af1e7
Yunseok Choi <ethdemor@gmail.com>
2014-06-03 00:37:06 +0900

    MySQL 세팅 추가
    python manage.py syncdb

--- springcamp2014/settings.py
+++ springcamp2014/settings.py
@@ -62,8 +62,10 @@ WSGI_APPLICATION = 'springcamp2014.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'springcamp2014',
+        'USER': 'springcamp',
+        'PASSWORD': 'test'
     }
 }
 
